a7738df81d4aafb117e529bfa4e8e5f457870f74
Raghava Seru <raghavamsc@gmail.com>
2021-10-10 01:55:19 -0400

    merging he code to main branch from feature1

--- sample.js
+++ sample.js
@@ -1,5 +1,9 @@
 console.log("My file1")
+<<<<<<< HEAD
 console.log("Adding another Commit on main branch..!")
 console.log("my first commit from feature2 branch..!")
 console.log("my second commit from feature2 branch..!")
 console.log("my third commit from feature2 branch..!")
+=======
+console.log("my second commit in feature1 branch")
+>>>>>>> f99506f (My second commit in to feature2 branch)
